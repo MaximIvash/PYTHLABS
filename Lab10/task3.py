0268bd55210656f57117bea7beb78c94b37fd45f
MaximIvash <cradd860@gmail.com>
2024-12-17 00:31:17 +0200

    [lab 10][task 1,2,3 added]

---
+++ Lab10/task3.py
@@ -0,0 +1,10 @@
+from docx import Document
+
+doc = Document('text.docx')
+table = doc.tables[0]
+Dict = {}
+for row in table.rows[1:]:
+    key = row.cells[0].text
+    value = row.cells[2].text
+    Dict[key] = value
+print(Dict)
